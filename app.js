d6f4189e7f14abe3a64aea5e363d7ac72db60fb4
Scarsgard-S <50478395+Scarsgard-S@users.noreply.github.com>
2024-01-24 14:20:09 +0300

    Добавить в app.js функцию поиска минимальной длины в строке

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//commit 1
 "use strict";
 
 function hello_js () {
@@ -8,8 +9,27 @@ hello_js();
 
 let b = true;
 
+// commit 2
 function booleanToString() {
     return b ? "true" : "false";
 }
 
 console.log(booleanToString(b));
+
+//commit 3
+let s = "Let's travel abroad shall we"
+
+function findShort(s) {
+    let words = s.split(' ');
+    let min = words[0].length;
+    
+    for (let word of words) {
+      if (word.length < min) {
+        min = word.length;
+      }
+    }
+    
+    return min;
+}
+
+console.log(findShort(s));
